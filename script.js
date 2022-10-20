2cabb7bc43bd2cf5a39b792f1725745ce791ed5c
JanusZhu <janus.zhu@yale.edu>
2022-10-21 01:07:47 +0900

    Added theme invertingand resizing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const root = document.querySelector(":root");
 let brightTheme = true;
-let fontSize = 15;
+let fontSize = 25;
 function invertTheme() {
   if (brightTheme === true) {
     brightTheme = !brightTheme;
